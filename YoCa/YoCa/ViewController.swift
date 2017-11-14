1ed0b2992f224d022c85910d8d24dda40e4bf34b
Carmen <carmen.bijlaart@hotmail.com>
2017-11-14 14:38:31 +0100

    IOS basic project

---
+++ YoCa/YoCa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoCa
+//
+//  Created by Carmen Bijlaart on 14-11-17.
+//  Copyright © 2017 Carmen Bijlaart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
