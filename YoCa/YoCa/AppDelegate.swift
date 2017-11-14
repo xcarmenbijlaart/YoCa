143dadc79c12537c0f186e140b4b0f98b2b1519b
Carmen <carmen.bijlaart@hotmail.com>
2017-11-14 21:37:41 +0100

    Firebase realtime database works!

--- YoCa/YoCa/AppDelegate.swift
+++ YoCa/YoCa/AppDelegate.swift
@@ -10,14 +10,18 @@ import UIKit
 import CoreData
 import Firebase
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+   
+    override init() {
+          FirebaseApp.configure()
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
         return true
     }
 
--- YoCa/YoCa/ViewController.swift
+++ YoCa/YoCa/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var txtviewInsert: UITextView!
+    @IBOutlet weak var btnSend: UIButton!
 
+    
+    //set the root reference with the child reference from the firebase database
+    let conditionref = Firebase.Database.database(url: "https://yoca-f0730.firebaseio.com").reference().child("condition")
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtviewInsert.layer.borderWidth = 1
+        txtviewInsert.layer.borderColor = UIColor.darkGray.cgColor
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //a checker, ook wel ophaler van de info uit de database
+        conditionref.observe(DataEventType.value, with: { (snapshot) in
+            self.labelText.text = snapshot.value as! String?
+        })
+      
+        
+    }
+    
+    
+    @IBAction func btnSendDidTouch(_ sender: Any) {
+        //change the value
+        if(txtviewInsert != nil){
+            
+           conditionref.setValue(txtviewInsert.text)
+        }
+        
+    }
+    
 }
 
